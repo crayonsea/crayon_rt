19005dc4650eef13b9a5df1e17cd85f6c1b13ee4
crayon <15651875728@163.com>
2021-04-17 22:13:11 +0800

    :sparkles: (crayon): 实现二次方程求解

--- include/core/crayon.h
+++ include/core/crayon.h
@@ -29,6 +29,24 @@ namespace crayon {
     template <typename T>
     class Point3;
 
+    // Utils
+    inline bool Quadratic(double A, double B, double C, double *t0, double *t1) {
+        double discrim = B * B - 4.0 * A * C;
+        if (discrim < 0.0) return false;
+        
+        double root_d = std::sqrt(discrim);
+        
+        double q;
+        if (B < 0) q = -0.5 * (B - root_d);
+        else q = -0.5 * (B + root_d);
+        
+        *t0 = q / A;
+        *t1 = C / q;
+        if (*t0 > *t1) std::swap(*t0, *t1);
+        
+        return true;
+    }
+
     
 } // namespace crayon
 
