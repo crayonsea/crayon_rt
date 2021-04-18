8aa02981a10b23d4855e67d0edff8216d4321a73
crayon <15651875728@163.com>
2021-04-18 18:47:51 +0800

    :sparkles: (scene): 添加场景封装

---
+++ include/core/scene.h
@@ -0,0 +1,39 @@
+#ifndef CRAYON_CORE_SCENE_H
+#define CRAYON_CORE_SCENE_H
+
+#include "core/crayon.h"
+#include "core/shape.h"
+
+
+namespace crayon {
+
+// 
+// Scene
+// 
+class Scene {
+public:
+    std::vector<std::shared_ptr<Shape>> objects;
+public:
+    Scene() {}
+    
+    void add(std::shared_ptr<Shape> object) {
+        objects.push_back(object);
+    }
+
+    bool intersect(const Ray &ray, Interaction *isect) const {
+        double t_hit = ray.tmax;
+        bool hitted = false;
+        for (auto object : objects) {
+            if (object.get()->intersect(ray, &t_hit, isect)) {
+                ray.tmax = t_hit;
+                hitted = true;
+            }
+        }
+        return hitted;
+    }
+
+};
+
+} // namespace crayon
+
+#endif
